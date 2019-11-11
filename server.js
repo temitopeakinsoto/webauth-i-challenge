a235c550cd65bd5af1b994c2e4aadfcad3583ff4
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-11 14:41:01 +0100

    Add api/users and api/register endpoints
    In this commit, I add the api/users and api/register endpoints. These endpoints are used to get the lists of all users from the database and register a new user respectively

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs')
+const Users = require('./users/users-model');
 
 
 const server = express();
@@ -13,5 +14,32 @@ server.use(cors());
 server.get('/', (req, res) => {
   res.send("It's alive!");
 });
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+        res.status(500).json({message: `There was an error: ${err.message}`});
+    });
+})
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 11)
+    const newUser = {
+      username: req.body.username,
+      password: hash,
+    };
+  
+    Users.add(newUser)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 module.exports = server;
