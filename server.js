4c331254bd79ff8761690fb8b9155ce560bb073e
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-11 14:49:35 +0100

    Add a login enpoint to Api
    In this commit, I added a post /api/login endpoint to the router. This endpoint checks to see if the username and password stored in the req.body matches the credentials in the database.

--- server.js
+++ server.js
@@ -15,6 +15,23 @@ server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
+server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
 server.get('/api/users', (req, res) => {
     Users.find()
     .then(users => {
