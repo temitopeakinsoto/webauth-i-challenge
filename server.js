df1f78490bcb8980765c13bbce0137ca4877772d
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-11 13:59:09 +0100

    Add server.js file for routing requests to our API endpoints

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs')
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send("It's alive!");
+});
+
+module.exports = server;
